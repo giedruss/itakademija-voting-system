0ba5e3042d09819dc12583ce9a116856868e6332
mindaugasd <mindaugas.dziautas@gmail.com>
2017-01-16 16:01:29 +0200

    constituency can be added in UI

--- src/main/resources/static/js/components/add_constituency_component.jsx
+++ src/main/resources/static/js/components/add_constituency_component.jsx
@@ -3,14 +3,18 @@ var AddConstituencyComponent = React.createClass({
         return (
                 <form>         
                 <label>Apygardos pavadinimas</label><br />
-                <input className="form-control" type="text" /><br />
-                <button className="btn btn-success" >Pridėti</button>
+                <input className="form-control" type="text" value={this.props.constituency.name} onChange={this.props.onFieldChange('name')} /><br />
+                <button className="btn btn-success" onClick={this.props.onAddClick}>Pridėti</button>
                 <button className="btn btn-danger" onClick={this.props.onCancel}>Atšaukti</button>
                 </form>
-                
-        
         )
     }
 });
+
+AddConstituencyComponent.propTypes = {
+        constituency: React.PropTypes.object.isRequired,
+        onFieldChange: React.PropTypes.func.isRequired,
+        onAddClick: React.PropTypes.func.isRequired
+};
 
 window.AddConstituencyComponent = AddConstituencyComponent;
--- src/main/resources/static/js/containers/add_constituency_container.jsx
+++ src/main/resources/static/js/containers/add_constituency_container.jsx
@@ -1,12 +1,43 @@
 var AddConstituencyContainer = React.createClass({
     
+    getInitialState: function() {
+        return {
+            constituency: {
+                name: ''
+            }
+        }
+    },
+    
+    handleFieldChange: function(fieldName) {
+        var self = this;
+        return function(e) {
+          var constituency = self.state.constituency;
+          constituency[fieldName] = e.target.value;
+          self.setState({ constituency: constituency });
+        };
+    },
+    
+    handleAddConstituency: function(e) {
+        e.preventDefault();
+        var self = this;
+        axios.post('/api/constituency', this.state.constituency).then(function () {
+            console.log('constituency added');
+            self.context.router.push('/con');
+          });
+    },
+    
     handleCancel: function() {
         this.context.router.push('/con');
     },
     
 
     render: function() {
-        return <AddConstituencyComponent onCancel={this.handleCancel}/>
+        return <AddConstituencyComponent 
+            constituency={this.state.constituency} 
+            onFieldChange={this.handleFieldChange}
+            onAddClick={this.handleAddConstituency}
+            onCancel={this.handleCancel} 
+              />
     }
 });
 
